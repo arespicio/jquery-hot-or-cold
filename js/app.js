c494cede1c5896e9eaf973a1ce31c712320bf35d
Ashleigh Respicio <ashleighrespicio@gmail.com>
2015-03-13 12:37:36 -0400

    Added new game functionality and logging guesses

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
 
 $(document).ready(function(){
 
-	//random number generated on load
-	var target = prompt(Math.floor((Math.random() * 100) + 1));
-
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -15,11 +12,72 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+    $("#guessButton").click(function() {
+      handleGuessButtonClick();
+    });
+
+    $('form').submit(function(event) {
+      event.preventDefault();
+    });
+
+    $(".new").click(function() {
+      newGame();
+    });
+
 });
 
-function newGame() {
-	location.reload();
+function handleGuessButtonClick() {
+  var number = $('#userGuess').val();
+  var guess = + number;
+  $('#guessList').append("<li>" + guess + "</li>");
+  $('#userGuess').val('');
 }
+
+function newGame() {
+  var target = Math.floor((Math.random() * 100) + 1);
+  $('#userGuess').val('');
+  $('#guessList').empty();
+}
+
+function guess() {
+  var diff = (target - guess);
+
+	if( diff >= 50){
+  		 	$('#feedback').replaceWith(function() {
+  		 		return "<h2>You are an ice cube!</h2>";
+  		 	});
+  		} else if( diff <= 30){
+  			$('#feedback').replaceWith(function() {
+  				return "<h2>You are slowly melting!</h2>";
+  			});
+  		} else if( diff > 20){
+  			$('#feedback').replaceWith(function() {
+  				return "<h2>You are slowly melting!</h2>";
+  			});
+		} else if( diff <= 20){
+  			$('#feedback').replaceWith(function() {
+  				return "<h2>You are a puddle of water!</h2>";
+  			});
+  		} else if( diff > 10){
+  			$('#feedback').replaceWith(function() {
+  				return "<h2>You are a puddle of water!</h2>";
+  			});
+  		} else if( diff <= 10){
+  			$('#feedback').replaceWith(function() {
+  				return "<h2>You are boiling water!</h2>";
+  			});
+  		} else if( diff >= 1){
+  			$('#feedback').replaceWith(function() {
+  				return "<h2>You are boiling water!</h2>";
+  			});
+  		} else if (guess === target){
+  			$('#feedback').replaceWith("YOU WIN!");
+  		}
+}
+
+
+
+
 
 
 
